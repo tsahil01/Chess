9234a399c0c67f0a6665164e99daae63e44904c1
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-04-19 11:13:48 +0530

    be v1

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,6 +1,6 @@
 import { WebSocket } from "ws";
 import { Chess } from 'chess.js'
-import { GAME_OVER, MOVE } from "./messages";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
 
 export class Game{
     public player1: WebSocket;
@@ -13,6 +13,21 @@ export class Game{
         this.player2 = player2;
         this.board = new Chess();
         this.startTime = new Date();
+
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: 'white',
+            }
+        }));
+
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: 'black',
+            }
+        }));
+
     }
 
     makeMove(socket: WebSocket, move: {
