dcae1c0c63b3f2020743c767905b3433069d720c
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-04-19 11:32:42 +0530

    some logs

--- backend/src/GameManager.ts
+++ backend/src/GameManager.ts
@@ -16,7 +16,6 @@ export class GameManager {
     addUser(socket: WebSocket){
         this.users.push(socket);
         this.addHandler(socket);
-
     }
 
     removeUser(socket: WebSocket){
@@ -29,18 +28,21 @@ export class GameManager {
             const message = JSON.parse(data.toString());
 
             if(message.type === INIT_GAME){
+        
                 if(this.pendingUser){
+                    console.log("Game started");
                     const game = new Game(this.pendingUser, socket);
                     this.games.push(game);
                     this.pendingUser = null;
 
                 } else {
+                    console.log("Pending user");
                     this.pendingUser =  socket;
                 }
             }
 
             if(message.type === MOVE){
-                
+
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
                 if(game){
                     game.makeMove(socket, message.move);
